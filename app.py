8aaeb34b37b778a42799dff941112b56f517cb46
Anatoly Raev <cralixraev@gmail.com>
2022-05-01 13:52:48 +0300

    Admin blueprint, Dockerfile, requirements.txt

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from flask import Flask
+from blueprints.admin.admin import admin
 
 app = Flask(__name__)
+
+app.register_blueprint(admin, url_prefix='/admin')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
---
+++ blueprints/admin/admin.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, redirect, url_for
+
+admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
+
+
+@admin.route('/')
+def index():
+    return redirect(url_for('admin.login'))
+
+
+@admin.route('/login/')
+def login():
+    return 'Welcome to login page'
