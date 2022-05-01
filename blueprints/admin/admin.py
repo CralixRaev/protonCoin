6357c1c036042e8e3851f449ce40af491280d03d
Anatoly Raev <cralixraev@gmail.com>
2022-05-01 21:53:28 +0300

    admin users page and other admin improvments

--- blueprints/admin/admin.py
+++ blueprints/admin/admin.py
@@ -1,11 +1,40 @@
-from flask import Blueprint, redirect, url_for
-from flask_login import login_required
+from functools import wraps
+
+import flask
+from flask import Blueprint, redirect, url_for, render_template, request
+from flask_login import login_required, current_user
+
+from db.models.user import UserQuery
 
 admin = Blueprint('admin', __name__, template_folder='templates', static_folder='static')
 
 
+def admin_required(func):
+    @wraps(func)
+    def decorated_view(*args, **kwargs):
+        if not current_user.is_admin:
+            return flask.abort(403)
+        return func(*args, **kwargs)
+
+    return decorated_view
+
+
 @admin.route('/')
 @login_required
+@admin_required
 def index():
-    return '!'
+    context = {
+        'title': 'Главная страница'
+    }
+    return render_template("admin/index.html", **context)
 
+
+@admin.route('/users/')
+@login_required
+@admin_required
+def users():
+    context = {
+        'title': 'Пользователи',
+        'users': UserQuery.get_all_users()
+    }
+    return render_template("admin/users.html", **context)
--- db/models/user.py
+++ db/models/user.py
@@ -7,11 +7,12 @@ from db.database import db
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True, nullable=False, index=True)
     login = db.Column(db.String(32), nullable=False, unique=True, index=True)
-    email = db.Column(db.String(64), nullable=False, unique=True, index=True)
+    email = db.Column(db.String(64), nullable=True, unique=True, index=True)
     name = db.Column(db.String(32), nullable=False)
     surname = db.Column(db.String(32), nullable=False)
     patronymic = db.Column(db.String(32), nullable=True)
     hashed_password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
 
     group_id = db.Column(db.Integer, db.ForeignKey("group.id"), nullable=True, index=True)
 
@@ -26,3 +27,7 @@ class UserQuery:
     @staticmethod
     def get_user_by_login(login) -> User:
         return User.query.filter((User.email == login) | (User.login == login)).first()
+
+    @staticmethod
+    def get_all_users() -> list[User]:
+        return User.query.all()
