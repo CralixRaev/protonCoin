bc8c3179a6af7db729b8ff19cf2c421d8ac80e34
Anatoly Raev <cralixraev@gmail.com>
2022-09-08 13:26:47 +0300

    serve favicon

--- app.py
+++ app.py
@@ -82,5 +82,11 @@ def main():
     app.run(debug=True, host='0.0.0.0', port=80)
 
 
+# TODO: Serve other sizes and webmanifest
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+
 if __name__ == "__main__":
     main()
