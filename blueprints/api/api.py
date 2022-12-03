1155ac00a292d0f547631614306f7e01fd9f010e
Anatoly Raev <cralixraev@gmail.com>
2022-12-03 23:23:17 +0300

    update requirements.txt and add auth to api

--- blueprints/api/api.py
+++ blueprints/api/api.py
@@ -1,5 +1,6 @@
 import flask
 from flask import Blueprint
+from flask_login import login_required
 from flask_restful import Api
 
 from blueprints.api.resources.achievement import AchievementList
@@ -9,11 +10,15 @@ from blueprints.api.resources.gift import GiftList
 from blueprints.api.resources.group import GroupList
 from blueprints.api.resources.transaction import TransactionList
 from blueprints.api.resources.user import UserList
+from util import teacher_or_admin_required
 
 api_blueprint = Blueprint('api', __name__)
-# todo: auth
+@api_blueprint.before_request
+@login_required
+@teacher_or_admin_required
+def before_request():
+    pass
 api = Api(api_blueprint)
-
 # api.add_resource(GiftList, '/gifts/')
 # api.add_resource(GiftElement, '/gifts/<gift_id>')
 api.add_resource(GroupList, '/groups/')
