cada2fb7cceed495510828a570c529dfefb785d7
Anatoly Raev <cralixraev@gmail.com>
2023-12-12 14:28:22 +0300

    Update debug_config.py

--- protonCoin/debug_config.py
+++ protonCoin/debug_config.py
@@ -2,9 +2,7 @@ from protonCoin.config import Config
 
 
 class DebugConfig(Config):
-    SQLALCHEMY_DATABASE_URI = (
-        "postgresql+psycopg2://marketpack:Zk.,k.Fhbye123@localhost/protoncoin"
-    )
+    SQLALCHEMY_DATABASE_URI = None
     SECRET_KEY = "64e499b91a1ad2bb85b291578cda74c1fa65a10788412e310947368c50e62240"
     SMARTCAPTCHA_SERVER_KEY = "8gGwmsw67sibzarVhkPl66JvhkJtu4iy6f2Ibw0A"
     SMARTCAPTCHA_CLIENT_KEY = "8gGwmsw67sibzarVhkPl7BJtbK1pML7kjIENUY1e"
